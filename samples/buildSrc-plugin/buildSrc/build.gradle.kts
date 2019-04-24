7284bcae6e8ab17bd812e5f5e169bfa46a58fc1f
Andrey Mischenko <andrey.mischenko@bandlab.com>
2019-04-25 01:27:05 +0800

    Bug sample: Cannot apply external plugin in precompiled script plugin

--- samples/buildSrc-plugin/buildSrc/build.gradle.kts
+++ samples/buildSrc-plugin/buildSrc/build.gradle.kts
@@ -1,16 +1,6 @@
 plugins {
     `kotlin-dsl`
 }
-
-gradlePlugin {
-    plugins {
-        register("greet-plugin") {
-            id = "greet"
-            implementationClass = "GreetPlugin"
-        }
-    }
-}
-
 repositories {
     jcenter()
 }
---
+++ samples/buildSrc-plugin/buildSrc/src/main/kotlin/greet.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    // Try to apply any external plugin
+    id("com.github.johnrengelman.shadow") version "5.0.0"
+}
+
+tasks {
+    //shadowJar {}
+}
